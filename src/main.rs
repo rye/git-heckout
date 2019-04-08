675a64567c2cfeba29eac1422a51b49d307c55b8
Kristofer Rye <kristofer.rye@gmail.com>
2019-04-08 14:27:00 -0500

    main: Add simple repository finder
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 extern crate distance;
+extern crate git2;
+
+fn find_repository() -> Result<git2::Repository, git2::Error> {
+	git2::Repository::open_from_env()
+}
 
 fn main() {
+	let repository = find_repository().expect("Could not find a repository");
+
+	println!("git-heckout: {:?}", repository.workdir().unwrap());
+
 	println!("{}", distance::levenshtein("blep", "blop"));
 }
